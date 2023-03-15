31e1f6265630db04b673e369daa47546f4e10849
earl-stephens <stephensearl@hotmail.com>
2023-03-15 15:58:56 -0600

    Setup unicorn test and class

---
+++ src/test/java/creaturesTest/UnicornTest.java
@@ -0,0 +1,16 @@
+package creaturesTest;
+import creatures.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UnicornTest {
+	static Unicorn unicorn;
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
