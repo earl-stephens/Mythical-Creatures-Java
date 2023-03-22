f65310f60ff7f71c5acd1a3dbf67525b1ee6c270
earl-stephens <stephensearl@hotmail.com>
2023-03-21 21:45:55 -0600

    Test class, test, code class, and code for medusa

---
+++ src/main/java/creatures/Medusa.java
@@ -0,0 +1,13 @@
+package creatures;
+
+public class Medusa {
+	private String name;
+	
+	public Medusa(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
---
+++ src/test/java/creaturesTest/MedusaTest.java
@@ -0,0 +1,22 @@
+package creaturesTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import creatures.Medusa;
+
+class MedusaTest {
+	Medusa medusa;
+	
+	@BeforeEach
+	void setUp() {
+		medusa =  new Medusa("Cassiopeia");
+	}
+	
+	@Test
+	void testItHasAName() {
+		assertEquals("Cassiopeia", medusa.getName());
+	}
+
+}
